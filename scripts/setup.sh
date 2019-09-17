7aadb075b3d15653f36271ef31a5809a79b5d281
owlfred <23skdu>
2019-09-17 15:20:55 -0700

    updating keys

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -eux
 yum update -y
+yum install -y wget
 yum clean all
-# Installing vagrant keys
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-cd ~/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-chown -R vagrant ~/.ssh
+
+# Installing vagrant key
+mkdir ~vagrant/.ssh
+chmod 700 ~vagrant/.ssh
+cd ~vagrant/.ssh
+wget --no-check-certificate 'https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 ~vagrant/.ssh/authorized_keys
+chown -R vagrant ~vagrant/.ssh
