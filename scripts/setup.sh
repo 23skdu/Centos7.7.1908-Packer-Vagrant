9aa282b27c010fd5c4cd0b0e9c4bae6d15fa237a
owlfred <23skdu>
2019-09-17 16:19:45 -0700

    making wget silent

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,6 @@ yum clean all
 mkdir ~vagrant/.ssh
 chmod 700 ~vagrant/.ssh
 cd ~vagrant/.ssh
-wget --no-check-certificate 'https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub' -O authorized_keys
+wget --quiet --no-check-certificate 'https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 ~vagrant/.ssh/authorized_keys
 chown -R vagrant ~vagrant/.ssh
