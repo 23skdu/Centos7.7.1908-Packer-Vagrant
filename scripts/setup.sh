2c9436623b865ea33189d7f3d88aca51b4d740ed
owlfred <23skdu>
2019-09-17 17:14:13 -0700

    updating packer scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,3 @@
 #!/bin/bash -eux
 yum update -y
-yum install -y wget
 yum clean all
-
-# Installing vagrant key
-mkdir ~vagrant/.ssh
-chmod 700 ~vagrant/.ssh
-cd ~vagrant/.ssh
-wget --quiet --no-check-certificate 'https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub' -O authorized_keys
-chmod 600 ~vagrant/.ssh/authorized_keys
-chown -R vagrant ~vagrant/.ssh
