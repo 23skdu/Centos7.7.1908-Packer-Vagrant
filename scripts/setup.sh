2b07a10090b116aa740644097ddf957c7afa2e32
owlfred <23skdu>
2019-09-23 12:20:01 -0700

    updating packer build

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -eux
-yum update -y
-yum clean all
+sudo rpm -i http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install bzip2 make perl kernel-devel dkms
+sudo systemctl start dkms
+sudo systemctl enable dkms
+sudo mkdir /media/guest
+sudo mount -o loop /tmp/VBoxGuestAdditions.iso /media/guest
+cd /media/guest
+sudo ./VBoxLinuxAdditions.run
+sudo yum update -y
+sudo yum clean all
